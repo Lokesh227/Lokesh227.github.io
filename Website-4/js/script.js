ea156abd49dbd45112df84d11a84db71b36e30bc
Lokesh <lokeshrajendiran6@gmail.com>
2020-12-06 01:00:52 +0530

    updated

---
+++ Website-4/js/script.js
@@ -0,0 +1,6 @@
+function profileFunction() {
+  document.getElementById("myProfile").classList.toggle("d-block");
+}
+function homeFunction() {
+  document.getElementById("myhome").classList.toggle("d-flex");
+}
